4a1c98bc862e7d4b753431ea67c89f2346195e02
kperroni <kenny.perroni@gmail.com>
2018-11-23 13:31:22 -0500

    Add project files.

---
+++ 300954759(Kim)_300825160(Perroni)_Client/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _300954759_Kim__300825160_Perroni__Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
